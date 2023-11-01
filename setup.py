7c36f24c8ca437e875b25c0dd247f7d4756cf657
croketillo <jose_carrizosa@hotmail.com>
2023-11-01 18:48:41 +0100

    package build

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="docker-info",
+    name="dockerinfo",
     version="1.0",
-    description="Extract docker container information".
+    description="Extract docker container information",
     author="croketillo",
     author_email="croketillo@gmail.com",
-    packages=["docker_info"],
+    packages=["dockerinfo"],
     install_requires=[
           'docker',
       ],
