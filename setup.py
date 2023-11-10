1d597bd225d1486403bc3fb14cee8b6805a88e98
croketillo <jose_carrizosa@hotmail.com>
2023-11-10 17:24:43 +0100

    Update README.md

--- setup.py
+++ setup.py
@@ -1,31 +1,118 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-#
-# dockerinfo - Docker container information extractor 
-
-
-from setuptools import setup
-import io
+import json
 from os import path
+from setuptools import setup, find_packages
+from sys import version_info
+
+VERSION = "0.1"
+CURR_PATH = "{}{}".format(path.abspath(path.dirname(__file__)), '/')
 
 
-here = path.abspath(path.dirname(__file__))
 
-with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+
+def path_format(file_path=None, file_name=None, is_abspath=False,
+                ignore_raises=False):
+    """
+    Get path joined checking before if path and filepath exist,
+     if not, raise an Exception
+     if ignore_raise it's enabled, then file_path must include '/' at end lane
+    """
+    path_formatted = "{}{}".format(file_path, file_name)
+    if ignore_raises:
+        return path_formatted
+    if file_path is None or not path.exists(file_path):
+        raise IOError("Path '{}' doesn't exists".format(file_path))
+    if file_name is None or not path.exists(path_formatted):
+        raise IOError(
+            "File '{}{}' doesn't exists".format(file_path, file_name))
+    if is_abspath:
+        return path.abspath(path.join(file_path, file_name))
+    else:
+        return path.join(file_path, file_name)
+
+
+def read_file(is_json=False, file_path=None, encoding='utf-8',
+              is_encoding=True, ignore_raises=False):
+    """Returns file object from file_path,
+       compatible with all py versiones
+    optionals:
+      can be use to return dict from json path
+      can modify encoding used to obtain file
+    """
+    text = None
+    try:
+        if file_path is None:
+            raise Exception("File path received it's None")
+        if version_info.major >= 3:
+            if not is_encoding:
+                encoding = None
+            with open(file_path, encoding=encoding) as buff:
+                text = buff.read()
+        if version_info.major <= 2:
+            with open(file_path) as buff:
+                if is_encoding:
+                    text = buff.read().decode(encoding)
+                else:
+                    text = buff.read()
+        if is_json:
+            return json.loads(text)
+    except Exception as err:
+        if not ignore_raises:
+            raise Exception(err)
+    return text
+
+
+def read(file_name=None, is_encoding=True, ignore_raises=False):
+    """Read file"""
+    if file_name is None:
+        raise Exception("File name not provided")
+    if ignore_raises:
+        try:
+            return read_file(
+                is_encoding=is_encoding,
+                file_path=path_format(
+                    file_path=CURR_PATH,
+                    file_name=file_name,
+                    ignore_raises=ignore_raises))
+        except Exception:
+            # TODO: not silence like this,
+            # must be on setup.cfg, README path
+            return 'NOTFOUND'
+    return read_file(is_encoding=is_encoding,
+                     file_path=path_format(
+                         file_path=CURR_PATH,
+                         file_name=file_name,
+                         ignore_raises=ignore_raises))
+
 
 setup(
-    name="dockerinfo",
-    version="0.1",
-    description="Docker container information extractor",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    name='dockerinfo',
+    version=VERSION,
+    license=read("LICENSE", is_encoding=False, ignore_raises=True),
+    packages=find_packages(),
+    description='Dockerinfo - Extract conainer info',
+    long_description=read("README.rst"),
+    author='croketillo',
+    author_email='croketillo@gmail.com',
     url='https://github.com/croketillo/dockerinfo',
-    author="croketillo",
-    author_email="croketillo@gmail.com",
-    license='GNU-3',
-    keywords=['docker', 'docker info', 'container'],
-    packages=["dockerinfo"],
-    install_requires=['docker'],
-    
+    keywords=['docker', 'dockerinfo', 'docker module', 'docker info'],
+    install_requires=[
+        'docker',
+    ],
+    setup_requires=['docker'],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pytest-html',
+        'pytest-dependency',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
