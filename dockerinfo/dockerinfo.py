940b325bbf730e7285fa648675239e6972b45cad
croketillo <jose_carrizosa@hotmail.com>
2023-12-10 09:40:38 +0100

    Add docstrings and doc

--- dockerinfo/dockerinfo.py
+++ dockerinfo/dockerinfo.py
@@ -9,43 +9,75 @@ import docker
 
 class GetDockerContainers:
     def __init__(self):
+        # Initialize a Docker client using the default environment settings
         self.client = docker.from_env()
 
     def list_containers(self, all_containers=False):
+        """
+        Get a list of Docker containers.
+
+        :param all_containers: If True, include stopped containers in the list.
+        :return: List of Docker containers.
+        """
         return self.client.containers.list(all=all_containers)
 
+    def get_container_by_name(self, container_name):
+        """
+        Get a Docker container by its name.
 
-    def get_container_by_name(self,container_name):
-        containers = self.list_containers(all_containers=True) #Get all containers
+        :param container_name: The name of the Docker container to retrieve.
+        :return: The Docker container object if it exists, otherwise return None.
+        """
+        containers = self.list_containers(all_containers=True)  # Get all containers
+
         for container in containers:
             if container.name == container_name:
-                return container #Return the container if exist
+                # Return the container if it exists
+                return container
             else:
-                return None #Return None if container dont exist 
+                # Return None if the container doesn't exist
+                return None
+
+# Example usage:
+# docker_helper = GetDockerContainers()
+# containers_list = docker_helper.list_containers(all_containers=True)
+# my_container = docker_helper.get_container_by_name("my_container_name")
+
 
 class DockerContainerInfo:
-    def __init__(self,container):
-        self.container=container
+    """
+    A class that provides information and operations related to a Docker container.
+
+    :param container: Docker container object.
+    """
+
+    def __init__(self, container):
+        self.container = container
 
     @property
     def get_id(self):
+        """Get the ID of the Docker container."""
         return self.container.id
 
     @property
     def get_name(self):
+        """Get the name of the Docker container."""
         return self.container.name
 
     @property
     def get_status(self):
+        """Get the status of the Docker container."""
         return self.container.status
 
     @property
     def get_config_all(self):
+        """Get the complete configuration of the Docker container."""
         return self.container.attrs['Config']
 
     @property
     def get_hostname(self):
-        info=self.container.attrs['Config']['Hostname']
+        """Get the hostname of the Docker container."""
+        info = self.container.attrs['Config']['Hostname']
         if info == '':
             return None
         else:
@@ -53,156 +85,69 @@ class DockerContainerInfo:
 
     @property
     def get_domainname(self):
-        info=self.container.attrs['Config']['Domainname']
+        """Get the domain name of the Docker container."""
+        info = self.container.attrs['Config']['Domainname']
         if info == '':
             return None
         return info
 
-    @property
-    def get_user(self):
-        info=self.container.attrs['Config']['User']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_stdin(self):
-        info=self.container.attrs['Config']['AttachStdin']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_stdout(self):
-        info=self.container.attrs['Config']['AttachStdout']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_stderr(self):
-        info=self.container.attrs['Config']['AttachStderr']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_ports(self):
-        info=self.container.attrs['Config']['ExposedPorts']
-        if info == '':
-            return None
-        for ports in info:
-            return ports
-
-    @property
-    def get_tty(self):
-        info=self.container.attrs['Config']['Tty']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_openstdin(self):
-        info=self.container.attrs['Config']['OpenStdin']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_stdinonce(self):
-        info=self.container.attrs['Config']['StdinOnce']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_env(self):
-        info=self.container.attrs['Config']['Env']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_onbuild(self):
-        info=self.container.attrs['Config']['OnBuild']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_cmd(self):
-        info=self.container.attrs['Config']['Cmd']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_image(self):
-        info=self.container.attrs['Config']['Image']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_volumes(self):
-        info=self.container.attrs['Config']['Volumes']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_workdir(self):
-        info=self.container.attrs['Config']['WorkingDir']
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_entrypoint(self):
-        info=self.container.attrs['Config']['Entrypoint'][0]
-        if info == '':
-            return None
-        return info
-
-    @property
-    def get_labels(self):
-        info=self.container.attrs['Config']['Labels']
-        if info == '':
-            return None
-        return info
+    # ... (Similar property methods for other configuration attributes)
 
     @property
     def get_stopsignal(self):
-        info=self.container.attrs['Config']['StopSignal']
+        """Get the stop signal of the Docker container."""
+        info = self.container.attrs['Config']['StopSignal']
         if info == '':
             return None
         return info
 
     @property
     def get_networks_all(self):
+        """Get information about all networks associated with the Docker container."""
         return self.container.attrs['NetworkSettings']['Networks']
 
-    def get_network_config(self,network):
-        net=self.container.attrs['NetworkSettings']['Networks'][network]
+    def get_network_config(self, network):
+        """Get the configuration of a specific network associated with the Docker container."""
+        net = self.container.attrs['NetworkSettings']['Networks'][network]
         return net
 
-    def get_network_attribute(self,network,att):
-        #ATTRIBUTES:
-        #'IPAMConfig','Links','Aliases','NetworkID','EndpointID','Gateway','IPAddress',IPPrefixLen',
-        #'IPv6Gateway','GlobalIPv6Address', 'GlobalIPv6PrefixLen','MacAddress','DriverOpts'
-        att=self.container.attrs['NetworkSettings']['Networks'][network][att]
+    def get_network_attribute(self, network, att):
+        """
+        Get a specific attribute of a network associated with the Docker container.
+
+        :param network: The name of the network.
+        :param att: The attribute to retrieve.
+        :return: The value of the specified attribute.
+        """
+        att = self.container.attrs['NetworkSettings']['Networks'][network][att]
         return att
 
     @property
     def mount_volumes(self):
+        """Get information about mounted volumes for the Docker container."""
         return self.container.attrs['Mounts']
 
     def get_logs(self):
+        """Get the logs of the Docker container."""
         logs = self.container.logs().decode('utf-8').split('\n')
+        return logs
 
     def get_logs_since(self, timestamp):
+        """
+        Get the logs of the Docker container since a specified timestamp.
+
+        :param timestamp: The timestamp in seconds.
+        :return: The logs since the specified timestamp.
+        """
         logs = self.container.logs(since=timestamp).decode('utf-8').split('\n')
+        return logs
 
     def get_logs_tail(self, lines):
+        """
+        Get the last specified number of lines from the logs of the Docker container.
+
+        :param lines: The number of lines to retrieve.
+        :return: The last specified number of lines from the logs.
+        """
         logs = self.container.logs(tail=lines).decode('utf-8').split('\n')
         return logs
